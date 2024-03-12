e0920665a849801528ee1063be2fe24ade8b4462
gautamkumar09 <147022198+gautamkumar09@users.noreply.github.com>
2024-03-12 23:41:25 +0530

    Add files via upload

---
+++ UnderScoreRemove.java
@@ -0,0 +1,8 @@
+public class UnderScoreRemove {
+  public static void main(String[] args) {
+    String name = "my_name_is_Gautam_kumar";
+    name = name.replace("_", " ");
+    System.out.println(name);
+  }
+
+}
