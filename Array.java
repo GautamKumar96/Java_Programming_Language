2f3a74c3cdd4a7e54d7a92eff5af030f3df114dd
gautamkumar09 <147022198+gautamkumar09@users.noreply.github.com>
2024-03-12 23:42:58 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,14 @@
+public class Array {
+  public static void main(String[] args) {
+    int[] marks = { 98, 97, 99, 94 };
+
+    int[][] finalmarks = { { 98, 97, 99, 94 }, { 98, 94, 97, 99, } };
+    System.out.println(finalmarks[0][1]);
+    System.out.println(finalmarks[0][2]);
+    System.out.println(finalmarks[0][3]);
+    System.out.println(finalmarks[1][0]);
+    System.out.println(finalmarks[1][1]);
+    System.out.println(finalmarks[0][0]);
+
+  }
+}
