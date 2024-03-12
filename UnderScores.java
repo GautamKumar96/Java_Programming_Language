b5681577734b9f0d5b10cd00ba248f9442d1785b
gautamkumar09 <147022198+gautamkumar09@users.noreply.github.com>
2024-03-12 23:37:59 +0530

    Add files via upload

---
+++ UnderScores.java
@@ -0,0 +1,8 @@
+public class UnderScores {
+  public static void main(String[] args)
+
+  {
+    String UnderScore = "my name is Gautam kumar ";
+    System.out.println(UnderScore.replace(" ", "_"));
+  }
+}
