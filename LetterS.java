a9ad1fac754e9a119d402adcb64b61ea47ea85f5
gautamkumar09 <147022198+gautamkumar09@users.noreply.github.com>
2024-03-13 11:02:08 +0530

    Add files via upload

---
+++ LetterS.java
@@ -0,0 +1,7 @@
+public class LetterS {
+  public static void main(String[] args) {
+    String myLetter = "dear sachin ,\n\t this java course is nice !,\n\t thanks";
+    System.out.println(myLetter);
+
+  }
+}
