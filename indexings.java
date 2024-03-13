6cda190060f55e31f88a38c3384d7585c51b151b
gautamkumar09 <147022198+gautamkumar09@users.noreply.github.com>
2024-03-13 10:46:35 +0530

    Add files via upload

---
+++ indexings.java
@@ -0,0 +1,9 @@
+public class indexings {
+  public static void main(String[] args) {
+    String myString = "i am  a java   developer  ";
+    System.out.println(myString.indexOf(" "));
+    System.out.println(myString.indexOf("   "));
+
+  }
+
+}
