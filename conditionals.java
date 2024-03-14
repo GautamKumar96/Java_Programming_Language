5509259d74e382fe0b29369cfe75e8e3f7619831
gautamkumar09 <147022198+gautamkumar09@users.noreply.github.com>
2024-03-14 22:32:11 +0530

    Add files via upload

---
+++ conditionals.java
@@ -0,0 +1,10 @@
+public class conditionals {
+  public static void main(String[] aStrings) {
+    int age =26;
+    if (age > 18) {
+      System.out.println("Yes boy you can vote! ");
+    } else {
+      System.out.println("No boy you cant vote!");
+    }
+  }
+}
