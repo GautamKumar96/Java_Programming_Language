41ca4e2659e4273035b85cf2c091315d7aaebeff
gautamkumar09 <147022198+gautamkumar09@users.noreply.github.com>
2024-03-15 13:21:39 +0530

    Add files via upload

---
+++ Logical.java
@@ -0,0 +1,19 @@
+public class Logical {
+  public static void main(String[] args){
+ 
+    boolean a = true;
+    boolean b = false;
+    if( a||b )
+    {
+      System.out.println("yes");
+
+    }
+    else{
+      System.out.println("No");
+    }
+  }
+
+}
+
+
+
