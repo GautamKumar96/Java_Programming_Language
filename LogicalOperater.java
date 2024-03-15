9cf88b7610599f49c3106970d2ded4107881c524
gautamkumar09 <147022198+gautamkumar09@users.noreply.github.com>
2024-03-15 11:20:24 +0530

    Add files via upload

---
+++ LogicalOperater.java
@@ -0,0 +1,16 @@
+public class LogicalOperater {
+  public static void main(String[] args){
+ 
+    boolean a = false;
+    boolean b = false;
+    if( a&&b )
+    {
+      System.out.println("yes");
+
+    }
+    else{
+      System.out.println("No");
+    }
+  }
+
+}
