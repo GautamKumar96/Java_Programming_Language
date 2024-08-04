8a0e3eef030b2d9022295894ec43a277a3bb07da
gautamkumar09 <147022198+gautamkumar09@users.noreply.github.com>
2024-08-05 00:33:09 +0530

    trueandFalse

---
+++ booleanFunction.java
@@ -0,0 +1,12 @@
+public class booleanFunction {
+  public static void main(String []args){
+    boolean a = false;
+    boolean b = true;
+    if(a||b){
+      System.out.println("Yes");
+    }
+    else{
+      System.out.println("No");
+    }
+  }
+}
