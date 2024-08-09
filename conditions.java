bdf89ce41957bb0e06b17b5324f4013c9592c131
gautamkumar09 <147022198+gautamkumar09@users.noreply.github.com>
2024-08-09 22:58:43 +0530

    conditional_statement

---
+++ conditions.java
@@ -0,0 +1,12 @@
+public class conditions {
+
+   public static void main(String [] args){
+    int age = 24;
+    if(age>18){
+      System.out.println("yes can drive");
+    }
+    else{
+      System.out.println("NO you can drive a car");
+    }
+   }
+}
