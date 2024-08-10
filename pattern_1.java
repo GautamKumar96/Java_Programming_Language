2a8036f1ec74182f305faf1120ba7e7c6a24209e
gautamkumar09 <147022198+gautamkumar09@users.noreply.github.com>
2024-08-10 10:37:49 +0530

    print pattern

---
+++ pattern_1.java
@@ -0,0 +1,15 @@
+public class pattern_1 {
+  static void pattern(int n){
+    for(int i=0; i<n; i++){
+      for(int j=0; j<i+1; j++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+
+  }
+  public static void main(String [] args){
+    int n =3;
+    pattern(n);
+  }
+}
