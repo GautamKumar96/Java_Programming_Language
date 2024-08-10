8c5bcc4cce7333a318f29bc04b0d230ae58e6c16
gautamkumar09 <147022198+gautamkumar09@users.noreply.github.com>
2024-08-10 10:17:12 +0530

    commit

---
+++ custom_class_2.java
@@ -0,0 +1,13 @@
+ class Employee{
+  int id ;
+  String name;
+ }
+public class custom_class_2 {
+public static void main(String[] args){
+  Employee Gautam = new Employee();
+  Gautam.id = 12; 
+  Gautam.name = "Gautam Kumar";
+  System.out.println(Gautam.name);
+  System.out.println(Gautam.id);
+}
+}
