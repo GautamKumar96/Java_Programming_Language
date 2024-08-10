6c4c7c30870105cadc423fd91585de503d1f297a
gautamkumar09 <147022198+gautamkumar09@users.noreply.github.com>
2024-08-10 10:22:24 +0530

    add two number

---
+++ add_num2.java
@@ -0,0 +1,9 @@
+public  class add_num2 {
+public static void main(String [] args){
+  int number1 = 100;
+  int number2 = 200;
+  int number3 = 300;
+  int sum  =  number1+ number2+number3;
+  System.out.println(sum);
+}
+}
