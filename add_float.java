dce43904c75bf3cd53fb43a4cecebec1c7fb2fa2
gautamkumar09 <147022198+gautamkumar09@users.noreply.github.com>
2024-08-10 10:21:42 +0530

    adding floating value

---
+++ add_float.java
@@ -0,0 +1,12 @@
+public class add_float {
+  public static void main(String [] args){
+    byte x = 5;
+    int y =  6;
+    short z=  8;
+    int a = x+y+z;
+    float b = 6.54F+x+y+z;
+    System.out.println(a);
+    System.out.println(b);
+  }
+}
+ 
