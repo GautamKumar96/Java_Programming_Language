808dd7d72b57d3e16f1e34ef2ea16a7755af278f
gautamkumar09 <147022198+gautamkumar09@users.noreply.github.com>
2024-08-10 10:30:13 +0530

    sub  string

---
+++ SubString.java
@@ -0,0 +1,8 @@
+public class SubString {
+  public static void main(String [] args){
+    String  name = "Gautam Kumar";
+    int value = name.length();
+    System.out.println(name.substring(7));
+
+  }
+}
