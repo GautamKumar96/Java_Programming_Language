edde1e91b0b509fd1b19dc9619e45ca54f3c8fb2
gautamkumar09 <147022198+gautamkumar09@users.noreply.github.com>
2024-08-10 10:10:30 +0530

    create own class

---
+++ custom_class_1.java
@@ -0,0 +1,13 @@
+class Employee{
+  int id ;
+  String name;
+}
+public class custom_class_1 {
+   public static void main(String[] args){
+    Employee  Gautam = new Employee();
+    Gautam.id = 17;
+     Gautam.name ="JAVA_CUSTOM";
+     System.out.println(Gautam.id);
+     System.out.println(Gautam.name);
+   }
+}
