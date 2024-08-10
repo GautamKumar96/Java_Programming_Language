cdc74baa7f2ea880fab711fd7876faf89b29f6fb
gautamkumar09 <147022198+gautamkumar09@users.noreply.github.com>
2024-08-10 10:29:40 +0530

    student information

---
+++ student_inf.java
@@ -0,0 +1,15 @@
+public class student_inf {
+  public static void main(String [] args){
+    byte age = 24;
+    char ch ='G';
+    //float f1 = 5.6;
+    double d1 = 4.66D;
+    boolean a = true;
+    System.out.println(age);
+    System.out.println(ch);
+    //System.out.println(f1);
+    System.out.println(d1);
+    System.out.println(a);
+    
+  }
+}
