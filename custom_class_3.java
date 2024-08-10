ac470e88c7c3de9dc615cc20485bb996569a6210
gautamkumar09 <147022198+gautamkumar09@users.noreply.github.com>
2024-08-10 10:17:45 +0530

    commit

---
+++ custom_class_3.java
@@ -0,0 +1,24 @@
+class Employee{
+  int  id ;
+  String name;
+  Double flouDouble;
+  String address;
+  String dept;
+}
+public class custom_class_3 {
+  public static void main(String[] args) {
+    Employee a = new Employee();
+    a.name =" name = Gautam  Kumar ";
+    a.name ="regstration number = 310521205017";
+    a.dept = "  department = information technology";
+    a.id =   17;
+    a.flouDouble = 2024.04;
+    a.address = " Address = nawada bihar";
+    System.out.println(a.name);
+    System.out.println(a.dept);
+    System.out.println(a.id);
+    System.out.println(a.flouDouble);
+    System.out.println(a.address);
+
+  }
+}
