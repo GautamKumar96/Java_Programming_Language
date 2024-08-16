812092bb30f07db41da1284c61a6bbede8af08dd
gautamkumar09 <147022198+gautamkumar09@users.noreply.github.com>
2024-08-16 14:52:13 +0530

    custom

---
+++ Animal_3.java
@@ -0,0 +1,19 @@
+class forest{
+  public void elephant(){
+ System.out.println("chinghandana..");
+  }
+  public void horse(){
+    System.out.println("hinhinana..");
+  }
+  public void monkey(){
+    System.out.println("khikhi");
+  }
+}
+public class Animal_3 {
+  public static void main(String [] args){
+    forest obj = new  forest();
+    obj.elephant();
+    obj.horse();
+    obj.monkey();
+  }
+}
