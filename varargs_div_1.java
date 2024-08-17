798fd43c9ff0e1f60b0dd5f541a83ebb2ff1272f
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2024-08-18 00:01:16 +0530

    commit

---
+++ varargs_div_1.java
@@ -0,0 +1,9 @@
+public class varargs_div_1 {
+  
+  static int div(int a,int b){
+    return a/b;
+  }
+  public static void main(String []args){
+  System.out.println("the division value of 20 and 4 is:"+div(20,4));
+  }
+}
