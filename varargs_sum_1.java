d2e278d4ee582dc12f4300bbae4e174f5ee63bd1
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2024-08-17 23:48:40 +0530

    commit

---
+++ varargs_sum_1.java
@@ -0,0 +1,10 @@
+public class varargs_sum_1 {
+
+  static int sum(int a,int b){
+    return a+b;
+  }
+
+  public static void main(String [] args){
+    System.out.println(" the sum of 4 and 8 is:"+sum(4,8));
+  }
+}
