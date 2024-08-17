77a795fc751e6d62e5a0c60ac1e038d4c6d3caba
Gautam Kumar <gautamkumar.it2021@dscet.ac.in>
2024-08-17 14:34:49 +0530

    hii

--- Calculator.java
+++ Calculator.java
@@ -1,37 +1,33 @@
 import java.util.Scanner;
-public class Calculator {
-  public static void main(String []args) {
-    Scanner sc = new Scanner(System.in);
 
-    System.out.println("Enter your frist number:");
+public class calculator {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter your first number:");
     int a = sc.nextInt();
-    
     System.out.println("Enter your second number:");
     int b = sc.nextInt();
-
-    System.out.println("Enter your operator (+,-,*,/):");
+    System.out.println("Enter your  operator:");
     char ch = sc.next().charAt(0);
+    switch (ch) {
 
-    switch (ch){
       case '+':
-      System.out.println("Result:"+(a+b));
-      break;
+        System.out.println(a + b);
+        break;
       case '-':
-      System.out.println("Result:"+(a-b));
-      break;
+        System.out.println(a - b);
+        break;
       case '*':
-      System.out.println("Result:"+(a*b));
-      break;
+        System.out.println(a * b);
+        break;
       case '/':
-      if(b!=0){
-       System.out.println("Result"+(a-b));
-      }
-      else{
-        System.out.println("Error Division by zero is not allowed.");
-      }
-      break;
+        System.out.println(a / b);
+        break;
+      case '%':
+        System.out.println(a % b);
+        break;
       default:
-      System.out.println("Wrong operator");
+        System.out.println("Wrong answer");
     }
   }
 }
