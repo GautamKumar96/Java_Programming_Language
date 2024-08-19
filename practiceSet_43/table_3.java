f43fa9f99f4b46aebe50a48ef364304b6af71d7e
Gautam Kumar <gautamkumar.it2021@dscet.ac.in>
2024-08-19 09:53:37 +0530

    commit

---
+++ practiceSet_43/table_3.java
@@ -0,0 +1,10 @@
+public class table_3 {
+  static void multi(int n){
+    for(int i=1; i<=10; i++){
+    System.out.format("%d*%d=%d\n",n,i,n*i);
+    }
+  }
+  public static void main(String[ ]args){
+  multi(17);
+  }
+}
