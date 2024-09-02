a4983fb74e4082e440b89fe4f9d572d3b99465f4
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2024-09-02 18:57:34 +0530

    commit

--- Animal_2.java
+++ Animal_2.java
@@ -1,47 +1,44 @@
 class Animal {
   String name;
 
-  // Constructor for the Animal class
+
   Animal(String name) {
       this.name = name;
   }
 
-  // Method to make a generic animal sound
+ 
   void makeSound() {
       System.out.println("Generic animal sound");
   }
 
-  // Method to display the name of the animal
+ 
   void display() {
-      System.out.println("This is the animal name: " + name);
+      System.out.println("This is the animal named: " + name);
   }
 }
 
-// The monkey class extends Animal
+
 class Monkey extends Animal {
 
-  // Constructor for the Monkey class
+
   Monkey(String name) {
-      super(name); // Call the parent class (Animal) constructor
+      super(name); 
   }
 
-  // Override the makeSound method
   @Override
   void makeSound() {
       System.out.println("Khee Khee");
   }
 }
 
-// Main class to test the inheritance
+
 public class Animal_2 {
   public static void main(String[] args) {
-      // Creating an object of the Monkey class
-      Monkey myMonkey = new Monkey("Bandar");
+      Monkey myMonkey = new Monkey("Babdar");
 
-      // Calling the display method of the base class
       myMonkey.display();
 
-      // Calling the overridden makeSound method of the derived class
+
       myMonkey.makeSound();
   }
 }
