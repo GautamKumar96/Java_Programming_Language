7ddd6b3a1991cd12b79e8410729e66e40d5d3477
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2024-09-02 17:54:45 +0530

    commit

---
+++ NaturalNumbers.java
@@ -0,0 +1,10 @@
+
+public class NaturalNumbers {
+
+  public static void main(String[] args) {
+      // Loop from 1 to 100 and print each number
+      for (int i = 1; i <= 100; i++) {
+          System.out.println(i + " ");
+      }
+  }
+}
