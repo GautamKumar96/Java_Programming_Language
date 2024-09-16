e383ab02a90b9d26e3ad8680f9752355a37bf2a7
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2024-09-16 11:43:00 +0530

    commit

---
+++ overloading_method_2.java
@@ -0,0 +1,8 @@
+public class overloading_method_2 {
+  static void gautam(){
+    System.out.println("our country is great!\n"+"Bharat!");
+  }
+  public static void main(String[] args){
+    gautam();
+  }
+}
