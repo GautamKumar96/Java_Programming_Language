e1ae0a540d9203781c92306eb669efce6b1595b1
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2024-10-16 13:36:53 +0530

    commit

---
+++ GameState.java
@@ -0,0 +1,21 @@
+class Game{
+  public int number;
+  private int noOfGuesses;
+  public int getNoOfGuesses(){
+    return noOfGuesses;
+  }
+  public int setNoOfGuesses(int noOfGuesses) {
+    this.noOfGuesses = noOfGuesses;
+}
+  void Game(int  n){
+    this.number= n;
+  }
+   void TakeUserInput()
+      boolean isCorrectNumber(){
+      }
+}
+public class GameState {
+  public static void main(){
+
+  }
+}
