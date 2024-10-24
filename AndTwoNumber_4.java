b52e9bf9cbb5414ceff755e5ef1cee2e282e3024
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2024-10-24 11:10:08 +0530

    commit

---
+++ AndTwoNumber_4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class AndTwoNumber_4 {
+   public static void  main(String[] args){
+    System.out.println("add two number");
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter frist number");
+    int a = sc.nextInt();
+    System.out.println("Enter the second number");
+    int b = sc.nextInt();
+    System.out.println("result");
+    int sum = a + b;
+    System.out.println(sum);
+   }
+}
