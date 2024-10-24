59d2c98828e6923be33bb4755dbc74876414a44c
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2024-10-24 11:04:35 +0530

    SquarePerimeter

---
+++ SquarePerimeter_3.java
@@ -0,0 +1,8 @@
+public class SquarePerimeter_3 {
+  public static void main(String[] args) {
+      int  sidelength = 5;
+      int perimeter =  4*sidelength;
+      System.out.println(perimeter);
+  }
+}
+
