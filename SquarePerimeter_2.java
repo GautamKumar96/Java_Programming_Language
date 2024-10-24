bf5cf13d7090da1af9f8f20276199057bc5a3a47
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2024-10-24 11:05:18 +0530

    commit

---
+++ SquarePerimeter_2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class SquarePerimeter_2 {
+  public static void main(String[] args){
+      Scanner sc =  new Scanner(System.in);
+      int  side = sc.nextInt();
+      System.out.println("result");
+      int  perimeter = 4*side;
+      System.out.println(perimeter);
+  }
+}
