4f2bf9f60907f62e6cecaf0b828959b344c45fbe
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2024-10-24 11:09:38 +0530

    commit

---
+++ AddTwoNumbers_2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class AddTwoNumbers_2 {
+   public static void main(String[] args){
+  Scanner sc = new Scanner(System.in);
+   System.out.println("Adding two number...");
+   int m = sc.nextInt();
+   int n = sc.nextInt();
+   int sum = m + n;
+   System.out.println(sum);
+}
+}
