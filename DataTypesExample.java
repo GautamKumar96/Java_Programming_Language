1cc3a9363b232b796f612fa2acd4e7596c18fd6f
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2024-10-26 15:17:37 +0530

    commit

---
+++ DataTypesExample.java
@@ -0,0 +1,27 @@
+public class DataTypesExample {
+  public static void main(String[] args) {
+    // Primitive data types
+    byte byteVar = 100;
+    short shortVar = 30000;
+    int intVar = 123456;
+    long longVar = 123456789L;
+    float floatVar = 12.34f;
+    double doubleVar = 12345.6789;
+    char charVar = 'A';
+    boolean boolVar = true;
+
+    // Non-primitive data types
+    String stringVar = "Hello, Java!";
+
+    // Printing values of all data types
+    System.out.println("byte value: " + byteVar);
+    System.out.println("short value: " + shortVar);
+    System.out.println("int value: " + intVar);
+    System.out.println("long value: " + longVar);
+    System.out.println("float value: " + floatVar);
+    System.out.println("double value: " + doubleVar);
+    System.out.println("char value: " + charVar);
+    System.out.println("boolean value: " + boolVar);
+    System.out.println("String value: " + stringVar);
+  }
+}
