f12eb9c13f3760bfc33a14bb0cf5d5e8868d6939
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2024-10-26 15:20:02 +0530

    commit

---
+++ OddNumber_2.java
@@ -0,0 +1,11 @@
+public class OddNumber_2 {
+  public static void main(String[]args){
+    int num =3;
+    if(num %2==0){
+      System.out.println("even number");
+    }
+    else{
+      System.out.println("odd number");
+    }
+  }
+}
