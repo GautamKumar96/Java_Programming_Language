1278343230a52313fa0eefe80a4dc980de567f24
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2024-10-26 15:12:25 +0530

    Quotient and Remainder

---
+++ QuotientAndRemainder.java
@@ -0,0 +1,35 @@
+public class QuotientAndRemainder {
+    public static void main(String[] args) {
+        // i) dividend = 72, divisor = 5
+        int dividend1 = 72;
+        int divisor1 = 5;
+        int quotient1 = dividend1 / divisor1;
+        int remainder1 = dividend1 % divisor1;
+
+        System.out.println("For dividend = 72 and divisor = 5:");
+        System.out.println("Quotient = " + quotient1);
+        System.out.println("Remainder = " + remainder1);
+        System.out.println();
+
+        // ii) dividend = 345, divisor = 10
+        int dividend2 = 345;
+        int divisor2 = 10;
+        int quotient2 = dividend2 / divisor2;
+        int remainder2 = dividend2 % divisor2;
+
+        System.out.println("For dividend = 345 and divisor = 10:");
+        System.out.println("Quotient = " + quotient2);
+        System.out.println("Remainder = " + remainder2);
+        System.out.println();
+
+        // iii) dividend = 345, divisor = 100
+        int dividend3 = 345;
+        int divisor3 = 100;
+        int quotient3 = dividend3 / divisor3;
+        int remainder3 = dividend3 % divisor3;
+
+        System.out.println("For dividend = 345 and divisor = 100:");
+        System.out.println("Quotient = " + quotient3);
+        System.out.println("Remainder = " + remainder3);
+    }
+}
