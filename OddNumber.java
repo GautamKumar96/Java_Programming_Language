a92359fc3bbf102dd53c9ad3665bac5b90bab789
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2024-10-26 15:19:33 +0530

    find the odd number

---
+++ OddNumber.java
@@ -0,0 +1,11 @@
+public class OddNumber{
+  public static void main(String[] args){
+    int num = 2;
+    if(num % 2!=0){
+    System.out.println("even number");
+    }
+    else{
+  System.out.println("Odd number");
+    }
+  }
+}
