8a21d582dcfa33caa9f473ebf0493dfab03ce6dc
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2024-11-03 21:37:19 +0530

    ConditionOperators_1

---
+++ ConditionalOperators.java
@@ -0,0 +1,12 @@
+public class ConditionalOperators {
+  public static void main(String []args){
+    boolean condition1 = 10>20;
+    boolean condition2 =  30>20;
+    
+    System.out.println("condition1&&condition2:"+(condition1&&condition2));
+    System.out.println("condition1||condition2:"+(condition1||condition2));
+    System.out.println("!condition1:"+(!condition1));
+    System.out.println("!condition2:"+(!condition2));
+
+  }
+}
