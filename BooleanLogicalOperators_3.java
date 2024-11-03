644a8044daf51df10202282a406cd18965a964e5
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2024-11-03 21:33:06 +0530

    Boolean_3

---
+++ BooleanLogicalOperators_3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class BooleanLogicalOperators_3 {
+  public static void main(String[]args){
+   Scanner sc = new Scanner (System.in);
+   System.out.println("Enter boolean value(true/false):");
+   System.out.println("Enter frist boolean value(true/false)");
+   boolean bool1 = sc.nextBoolean();
+   System.out.println("Enter second boolean value(true/false)");
+    boolean bool2 = sc.nextBoolean();
+  
+    System.out.println("bool1&&bool2:"+(bool1&&bool2));
+     System.out.println("bool1||bool2:"+(bool1||bool2));
+     System.out.println("!bool1"+(bool1));
+     System.out.println("!bool2"+(!bool2));
+
+   
+  }
+}
