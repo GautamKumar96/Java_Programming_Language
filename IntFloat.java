3f2d9c700c860c887fa224b0cd7f9fd9445d6d69
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2025-03-22 14:52:03 +0530

    Add files via upload
    int and floating values

---
+++ IntFloat.java
@@ -0,0 +1,8 @@
+public class IntFloat {
+  public static void main(String []args){
+    int num = 10;
+    float pi=  3.1416f;
+    System.out.println("integer value: "+num);
+    System.out.println("floting values: "+pi);
+  }
+}
