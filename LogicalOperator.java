4371a69790090c70a1c8ddd76726b478dbad17d8
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2025-03-22 14:53:14 +0530

    Add files via upload
    Logical operators in Java are used to combine multiple conditions and return a boolean (true or false) result.

---
+++ LogicalOperator.java
@@ -0,0 +1,11 @@
+public class LogicalOperator {
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 40;
+   // System.out.println(a+5);
+   // System.out.println(a+=10);
+   // System.out.println(a-=5);
+    System.out.println(a*=3);
+    System.out.println(b/=2);
+  }
+}
