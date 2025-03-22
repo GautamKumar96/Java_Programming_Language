9384c85ef2cef8d3d77ca198bfb48e840fddeb25
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2025-03-22 14:51:02 +0530

    Add files via upload
    An if-else statement in Java is used to execute different blocks of code based on a condition.

---
+++ if_else_2.java
@@ -0,0 +1,10 @@
+public class if_else_2 {
+  public static void main(String[] args) {
+    int num = 45;
+    if(num>0){
+       System.out.println("positive number");
+    } else{
+      System.out.println("negative number");
+    }
+  }
+}
