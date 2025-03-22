7c6161056f640a21192715bfd283fd850b1ac9a7
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2025-03-22 15:04:30 +0530

    Add files via upload
    Added Car class with model and year attributes, and a main program to initialize and display car details_3

---
+++ moter_3.java
@@ -0,0 +1,15 @@
+class car{
+  String model;
+  int year;
+}
+public class moter_3 {
+  public static void main(String[]args){
+    car MYcar = new car();
+    MYcar.model = "TATA";
+    MYcar.year = 2025;
+
+   System.out.println(MYcar.model);
+   System.out.println(MYcar.year);
+    
+  }
+}
