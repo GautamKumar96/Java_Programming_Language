c3d2e1173d9ed614652c3fa9b53efa4cda69c4f9
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2025-03-23 13:53:09 +0530

    Add files via upload
    Relational Operators is used to the compare two values

---
+++ relational_Operators.java
@@ -0,0 +1,14 @@
+public class relational_Operators {
+  public static void main(String[]args){
+    int a = 50;
+    int b = 20;
+    System.out.println("a: " + a + "b: " +  b);
+    System.out.println("a==b:"+(a==b));
+    System.out.println("a!=b:"+(a!=b));
+    System.out.println("a>b:"+(a>b));
+    System.out.println("a<b:"+(a<b));
+    System.out.println("a>=b:"+(a>=b));
+    System.out.println("a<=b:"+(a<=b));
+    System.out.println("");
+  }
+}
