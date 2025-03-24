382c1b046de69b308c8d55bda8a8b9fca6209d89
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2025-03-25 00:22:51 +0530

    Add files via upload
    A variable is a container that holds data during the execution of a Java program.

---
+++ Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+  public static void main(String[] args) {
+       int num = 10;
+       //float price = 9.99;
+      double  rate = 99.99;
+      char letter = 'G';
+      boolean suvah =  false;
+       String name = "java";
+       System.out.println(num );
+       System.out.println(rate);
+       System.out.println(letter);
+       System.out.println(suvah);
+       System.out.println(name);
+  }}
+
