9f1160b8d2266df0d3961e6a1ea242c544142459
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2025-03-26 00:10:12 +0530

    Add files via upload
    In Java, primitive data types are the most basic types of data and are not objects. They are predefined by the language and serve as the building blocks for data manipulation. Java has eight primitive data types

--- DataTypesExample.java
+++ DataTypesExample.java
@@ -1,27 +1,21 @@
 public class DataTypesExample {
   public static void main(String[] args) {
-    // Primitive data types
-    byte byteVar = 100;
-    short shortVar = 30000;
-    int intVar = 123456;
-    long longVar = 123456789L;
-    float floatVar = 12.34f;
-    double doubleVar = 12345.6789;
-    char charVar = 'A';
-    boolean boolVar = true;
+      byte b = 100;
+      short s = 30000;
+      int num = 123456;
+      long bigNum = 123456789L;
+      float pi = 3.14f;
+      double precisePi = 3.1415926535;
+      char letter = 'G';
+      boolean isJavaFun = true;
 
-    // Non-primitive data types
-    String stringVar = "Hello, Java!";
-
-    // Printing values of all data types
-    System.out.println("byte value: " + byteVar);
-    System.out.println("short value: " + shortVar);
-    System.out.println("int value: " + intVar);
-    System.out.println("long value: " + longVar);
-    System.out.println("float value: " + floatVar);
-    System.out.println("double value: " + doubleVar);
-    System.out.println("char value: " + charVar);
-    System.out.println("boolean value: " + boolVar);
-    System.out.println("String value: " + stringVar);
+      System.out.println("byte: " + b);
+      System.out.println("short: " + s);
+      System.out.println("int: " + num);
+      System.out.println("long: " + bigNum);
+      System.out.println("float: " + pi);
+      System.out.println("double: " + precisePi);
+      System.out.println("char: " + letter);
+      System.out.println("boolean: " + isJavaFun);
   }
 }
