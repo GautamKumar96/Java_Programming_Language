b16839435c884f787f21a8fd4e2ff3eb0f05e665
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2025-04-28 10:56:48 +0530

    commit

---
+++ Boolean_literals.java
@@ -0,0 +1,16 @@
+
+
+public class Boolean_literals {
+  public static void main(String[] args) {
+    //boolean x = false;
+   // boolean x = true;
+   int x = 0;
+    if(x)
+    {
+      System.out.println("hello");
+    }
+    else {
+      System.out.println("hi");
+    }
+  }
+}
