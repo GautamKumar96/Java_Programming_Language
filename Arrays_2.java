f0842123bd3a97e47195af45625cafc8d813faac
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2025-04-28 11:02:42 +0530

    commit

---
+++ Arrays_2.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+public class Arrays_2 {
+  public static void main(String[] args) {
+      String []  day  = {"raviwar","somwar","mangalwar"};
+    System.out.println(Arrays.toString(day));
+
+  }}
+
