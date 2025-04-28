0b1f8bfdf6d50c9d5afbe2157b390ce7afd72e70
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2025-04-28 11:00:02 +0530

    commit

---
+++ Arrays_7.java
@@ -0,0 +1,10 @@
+public class Arrays_7 {
+  public static void main(String[] args) {
+    int [] num = {11,13,14,15,16};
+    int sum = 0;
+    for(int i=0; i<num.length; i++){
+      sum = sum+num[i];
+    }
+    System.out.println(sum);
+  }
+}
