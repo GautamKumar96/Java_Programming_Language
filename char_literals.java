56f92026a70b74f05ab5adecf254ac036df6628c
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2025-04-28 10:53:57 +0530

    commit

---
+++ char_literals.java
@@ -0,0 +1,19 @@
+public class char_literals {
+  public static void main(String[] args) {
+    //char ch = 'a';
+    //char  ch = "a";
+    //char ch = 'ab';
+   // char ch =  97;
+   char ch = 65533;
+   char ca = 65534;
+   char cn = 65535;
+   char gk = 197;
+   char  km = 1970;
+   System.out.println(km);
+    System.out.println(ch);
+    System.out.println(ca);
+    System.out.println(cn);
+    System.out.println(gk);
+  }
+}
+  
