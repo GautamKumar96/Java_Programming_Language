6f90270ae2aa004279e78eeb100827f4e4bc8d5d
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2025-04-28 11:02:12 +0530

    commit

---
+++ Arrays_3.java
@@ -0,0 +1,9 @@
+
+import java.util.Arrays;
+
+public class Arrays_3 {
+  public static void main(String[] args) {
+      String []   mobile = {"realme", "oppo","poco"};
+      System.out.println(Arrays.toString(mobile));
+  }
+}
