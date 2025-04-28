135617542ecc1206739e2c4efaf3978a47a0b320
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2025-04-28 10:53:29 +0530

    commit

---
+++ char_literals_5.java
@@ -0,0 +1,18 @@
+public class char_literals_5 {
+  public static void main(String[] args) {
+    char ch = '\u0061';
+    char ca = '\u0062';
+    char cn = '\u0063';
+    char cb = '\u0064';
+    char ab =  '\u0065';
+    char as =  '\u0066';
+    int x = 0B1111;
+    System.out.println(ch);
+    System.out.println(ca);
+    System.out.println(cn);
+    System.out.println(cb);
+    System.out.println(ab);
+    System.out.println(as);  
+    System.out.println(x);
+  }
+}
