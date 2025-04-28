5f2da9f613adc0cf83d63b1069b586e86c3d542f
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2025-04-28 10:49:22 +0530

    commit

---
+++ hexa_decimal_1.java
@@ -0,0 +1,14 @@
+public class hexa_decimal_1 {
+   public static void main (String []args){
+    int a =  0X10;//(1*16)+(0*1) = 16
+    int b =  0x18a;//(1*16^2))+(8*16^1)+(10) +394
+    int c = 0X1be;
+    int d = 0X789;
+    int e = 0XBee;
+    System.out.println(e);
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+   }
+}
