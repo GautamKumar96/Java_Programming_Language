85b3bf0ab8aae22619943a7195b47417347a95ba
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2025-04-28 11:05:18 +0530

    commit

---
+++ actol_from_2.java
@@ -0,0 +1,9 @@
+public class actol_from_2 {
+  public static void main(String[] args) {
+    int a = 0111;//(0*8^3)+(1*8^2)+(1*8^1)+(1*8^0) = 73
+    int b = 011;//(0*8^2)+(1*8^1)+(1*8^0) = 9
+
+    System.out.println("a.." +a+ "b.." +b);
+  }
+}
+ 
