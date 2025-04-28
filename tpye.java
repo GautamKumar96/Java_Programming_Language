4f42f3f278ba2690bbcc05d2361124d500a252b9
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2025-04-28 10:13:09 +0530

    commit

---
+++ tpye.java
@@ -0,0 +1,32 @@
+
+public class tpye{
+public static void main(String[] args) {
+  //int s = 2147483648;
+ // double a = 10.4;
+   //byte a = true;
+  //System.out.println(b);
+  //System.out.println(a);
+  //int a = 128;
+  //byte b = 10.5;
+  //byte a = "durga";
+  //int b =  -127;
+  //Short s = 32768;
+  //Short s  = 20.5;
+  //Short s = true;
+ // int s = 214748367;
+  //long s = 126000*60;
+   //long s = f.lenght();
+   //boolean b = true;
+   //boolean b = True;
+  //System.out.println(b);
+  /*int n = 0XBeea;
+  int b = 0XAbcef;
+  System.out.println(n);
+  System.out.println(b);*/
+/*int s =  0100;
+System.out.println(s);*/
+//int s = 0x10;
+int  n = 0Xbeec;
+System.out.println(n);
+}
+}
