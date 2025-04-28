bb58ee472f3186cd0efdd227ea63f5771ea9e52b
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2025-04-28 11:01:45 +0530

    commit

---
+++ Arrays_4.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Arrays_4 {
+  public static void main(String []args){
+    String[]  n  = {"Name"};
+    String   []x = { "Gautam"};
+    String   a[] = {"Kumar"};
+    System.out.print(Arrays.toString(n));
+    System.out.print(Arrays.toString(x));
+    System.out.print(Arrays.toString(a));
+  }
+}
