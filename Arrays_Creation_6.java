0356843c08a9d3470caf4536622cb381c7fb6b23
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2025-04-30 11:00:00 +0530

    commit
    Add Java code testing array creation with negative size

---
+++ Arrays_Creation_6.java
@@ -0,0 +1,7 @@
+public class Arrays_Creation_6 {
+  public static void main(String[] args) {
+    int []  a =  new int [-5];
+   // System.out.println(args.length);
+   System.out.println(a.getClass().getName());
+  }
+}
