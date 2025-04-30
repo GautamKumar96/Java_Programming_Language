f9e4209383fe3d48bff6cbf197f6934a788a1d2b
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2025-04-30 10:49:13 +0530

    commit

--- Arrays_Creation_4.java
+++ Arrays_Creation_4.java
@@ -1,14 +1,17 @@
 public class Arrays_Creation_4 {
   public static void main(String[] args) {
+    int [] z = new int[0];
         int []  a = new int[3];
        short [] b= new short [3];
          byte[] c = new byte[2];
          boolean [] d = new boolean[3];
         String [] e = new String[3];
+        System.out.println(z.getClass().getName());
        System.out.println(a.getClass().getName());
        System.out.println(b.getClass().getName());
        System.out.println(c.getClass().getName());
       System.out.println(d.getClass().getName());
       System.out.println(e.getClass().getName());
+
 
   }}
