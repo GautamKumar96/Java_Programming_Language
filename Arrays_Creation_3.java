c8350ce85f248a4ac4d6ae579be845227934cb47
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2025-04-30 10:48:01 +0530

    commit

---
+++ Arrays_Creation_3.java
@@ -0,0 +1,11 @@
+
+
+public class Arrays_Creation_3 {
+  public static void main(String[] args) {
+    int [] [] x = new int [2] [];
+    System.out.println(x);
+    System.out.println(x[0]);
+    System.out.println(x[0][0]);
+  }
+}
+ 
