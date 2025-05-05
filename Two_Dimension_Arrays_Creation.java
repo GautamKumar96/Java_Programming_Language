8406f2cc071470a6e9c1a153c07686209627d7b3
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2025-05-05 13:58:14 +0530

    commit

---
+++ Two_Dimension_Arrays_Creation.java
@@ -0,0 +1,9 @@
+public class Two_Dimension_Arrays_Creation {
+  public static void main(String[]args){
+    int [][] X =  new int [2][];
+    X[0] = new int[2];
+    X[1] = new int[3];
+  System.out.println(X.getClass().getName());
+ // System.out.println(X);
+  } 
+}
