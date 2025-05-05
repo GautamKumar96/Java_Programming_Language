2a1363f8ac44aeaacb07e8eb11de804a7b11593e
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2025-05-05 14:02:30 +0530

    commit

---
+++ Array_Assignment.java
@@ -0,0 +1,12 @@
+public class Array_Assignment{
+  public static void main(String[] args) {
+      int [] x = new int[3];
+      x[0] = 9;
+      x[1] = 10;
+      x[2] = 11;
+      for(int i=0;i<x.length;i++){
+      
+        System.out.println(x[i]);
+      }
+  }
+}
