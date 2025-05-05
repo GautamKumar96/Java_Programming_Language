1c4182777d1979b1200ccff3c25c0f7968ce10be
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2025-05-05 14:27:57 +0530

    commit

---
+++ Array_without_loop.java
@@ -0,0 +1,9 @@
+public class Array_without_loop {
+  public static void main(String[] args) {
+      int  num [] = {50,69,59};
+      System.out.println(num[0]);
+      System.out.println(num[1]);
+      System.out.println(num[2]);
+      }
+  }
+
