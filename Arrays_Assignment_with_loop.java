06e36b00588d9ccf70fc258fc83548ef7abaeec5
Gautam Kumar <147022198+GautamKumar96@users.noreply.github.com>
2025-05-05 14:34:54 +0530

    commit

---
+++ Arrays_Assignment_with_loop.java
@@ -0,0 +1,8 @@
+public class Arrays_Assignment_with_loop {
+  public static void main(String[] args){
+    int num []  = {10,20,30,40,50,60,70,80};
+    for(int i =0; i<num.length;i++){
+      System.out.println(num[i]);
+    }
+    }
+}
